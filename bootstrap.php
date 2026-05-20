02548c614e25bea56b401709b0d559805ff624a1
ValerioP8 <valeriopalma15@gmail.com>
2026-05-20 13:45:51 +0200

    Added MySQL connection to Doctrine.
    Added a database connection test script.
    
    Local database connectivity test succeded.

--- bootstrap.php
+++ bootstrap.php
@@ -9,23 +9,21 @@ $config = ORMSetup::createAttributeMetadataConfiguration(
     paths: [__DIR__ . '/src'],
     isDevMode: true,
 );
-// or if you prefer annotation, YAML or XML
-// $config = ORMSetup::createAnnotationMetadataConfiguration(
-//    paths: array(__DIR__."/src"),
-//    isDevMode: true,
-// );
-// $config = ORMSetup::createXMLMetadataConfiguration(
-//    paths: [__DIR__ . '/config/xml'],
-//    isDevMode: true,
-//);
-// $config = ORMSetup::createYAMLMetadataConfiguration(
-//    paths: array(__DIR__."/config/yaml"),
-//    isDevMode: true,
-// );
-// configuring the database connection
-$connection = DriverManager::getConnection([
-    'driver' => 'pdo_sqlite',
-    'path' => __DIR__ . '/db.sqlite',
-], $config);
-// obtaining the entity manager
+
+// the connection configuration
+$dbParams = [
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => '',
+    'dbname'   => 'websiteDB',
+];
+
+//Fetch entity from:
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: [__DIR__ . '/src'],
+    isDevMode: true,
+);
+
+// on PHP < 8.4, use ORMSetup::createAttributeMetadataConfiguration() instead
+$connection = DriverManager::getConnection($dbParams, $config);
 $entityManager = new EntityManager($connection, $config);
---
+++ foundation/check-DBconnection.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Exception;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Configurazione connessione DB
+$connectionParams = [
+    'dbname'   => 'websiteDB',
+    'user'     => 'root',
+    'password' => '',
+    'host'     => '127.0.0.1',  //localhost
+    'port'     => 3306,         //utilizzando la porta default
+    'driver'   => 'pdo_mysql',
+];
+
+try {
+    // Crea connessione Doctrine DBAL
+    $conn = DriverManager::getConnection($connectionParams);
+
+    // Verifica connessione
+    $conn->connect();
+
+    if ($conn->isConnected()) {
+        echo "Database raggiungibile.\n";
+        //Scrivi 1 se funziona tutto correttamente (lato DB)
+        $result = $conn->executeQuery('SELECT 1');
+        echo $result->fetchOne();
+        echo "\n";
+    } else {
+        echo "Impossibile connettersi al database.\n";
+    }
+
+} catch (Exception $e) {
+    echo "Errore di connessione al database:\n";
+    echo $e->getMessage() . "\n";
+}
