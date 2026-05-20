c44d4e8e710cc66b0cd0eb5d7dfe424af78eea40
ValerioP8 <valeriopalma15@gmail.com>
2026-05-20 13:50:11 +0200

    Minor fix: Deleted accidental leftover copy.

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,6 @@ use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 require_once "vendor/autoload.php";
-// Create a simple "default" Doctrine ORM configuration for Attributes
-$config = ORMSetup::createAttributeMetadataConfiguration(
-    paths: [__DIR__ . '/src'],
-    isDevMode: true,
-);
 
 // the connection configuration
 $dbParams = [
